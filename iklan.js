c84a11845ed61ab049709bf07aaaa994f3e0878c
phinokio <phoenix_hunter182@yahoo.com>
2025-12-26 15:52:49 +0700

    Update iklan.js

--- iklan.js
+++ iklan.js
@@ -1,16 +1,1 @@
-<script type='text/javascript'>
-$(document).ready(function() {$('img#closed').click(function(){$('#bl_banner').hide(90);});});
-</script>
-<div id='fixedban' style='width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:999;-webkit-transform:translateZ(0);'>
-<div>
-<a id='close-fixedban' onclick='document.getElementById("fixedban").style.display = "none";' style='cursor:pointer;'><img alt='close' src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgwxAMYvxZdzaoHZdVhUFIoJwgi5Fdh8tLi88NK59g10m9HUKMjBODEnWwV-QIze4a2FXM3SfGhyphenhyphenFmLSqMVsa2cJ1gS1dRdX2tmouwR5dvmQUu3mZ37FRrPIrvn7xB8kx4tSKiU0sNs3G5v/s1600/btn_close.gif' title='close button' style='vertical-align:middle;'/></a></div>
-<div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
-
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-8197102543297996"
-     data-ad-slot="3724558263"
-     data-ad-format="rectangle"></ins>
-
-</div>
-</div>
+<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',3345436,document.body||document.documentElement)</script>
